a3787e0f2359d90a3affe091ee246d59b6612a60
yoni <johnysalvadorcampos@gmail.com>
2024-11-23 09:54:22 -0600

    Corregido la ruta en server.ts y actualizado el build

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,9 @@ import { dirname, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
 
 const serverDistFolder = dirname(fileURLToPath(import.meta.url));
-const browserDistFolder = resolve(serverDistFolder, '../browser');
+const browserDistFolder = resolve(serverDistFolder, '../dist/sistema-educativo/browser');
+
+
 
 const app = express();
 const angularApp = new AngularNodeAppEngine();
